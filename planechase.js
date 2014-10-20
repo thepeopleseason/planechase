83ba8ab27d1324532ad5d2e77689d7476adab7bd
James Hsiao <matchstick@gmail.com>
2014-10-20 15:56:48 -0400

    added planes object

--- planechase.js
+++ planechase.js
@@ -1,4 +1,91 @@
 var eternity = {
+  'names': {
+    '198062.jpg': { 'name': "The Maelstrom" },
+    '198063.jpg': { 'name': "Stronghold Furnace" },
+    '198064.jpg': { 'name': "Panopticon" },
+    '198065.jpg': { 'name': "Izzet Steam Maze" },
+    '198066.jpg': { 'name': "Feeding Grounds" },
+    '198067.jpg': { 'name': "The Hippodrome" },
+    '198068.jpg': { 'name': "Glimmervoid Basin" },
+    '198069.jpg': { 'name': "Tazeem" },
+    '198070.jpg': { 'name': "Naar Isle", 'counter': true },
+    '198071.jpg': { 'name': "Grixis" },
+    '198073.jpg': { 'name': "Academy at Tolaria West" },
+    '198074.jpg': { 'name': "Murasa" },
+    '198075.jpg': { 'name': "Sokenzan" },
+    '198076.jpg': { 'name': "Raven's Run" },
+    '198077.jpg': { 'name': "Cliffside Market" },
+    '198078.jpg': { 'name': "Minamo" },
+    '198079.jpg': { 'name': "Sanctum of Serra" },
+    '198080.jpg': { 'name': "Pools of Becoming" },
+    '198081.jpg': { 'name': "Velis Vel" },
+    '198083.jpg': { 'name': "The Dark Barony" },
+    '198084.jpg': { 'name': "Immersturm" },
+    '198085.jpg': { 'name': "Turri Island" },
+    '198086.jpg': { 'name': "Shiv" },
+    '198087.jpg': { 'name': "Goldmeadow" },
+    '198088.jpg': { 'name': "The Great Forest" },
+    '198090.jpg': { 'name': "Agyrem" },
+    '198092.jpg': { 'name': "The Eon Fog" },
+    '198095.jpg': { 'name': "Otaria" },
+    '198096.jpg': { 'name': "Isle of Vesuva" },
+    '198097.jpg': { 'name': "Fields of Summer" },
+    '198098.jpg': { 'name': "The AEther Flues" },
+    '198099.jpg': { 'name': "Bant" },
+    '198100.jpg': { 'name': "Krosa" },
+    '198101.jpg': { 'name': "Skybreen" },
+    '198102.jpg': { 'name': "The Fourth Sphere" },
+    '198103.jpg': { 'name': "Naya" },
+    '198105.jpg': { 'name': "Lethe Lake" },
+    '198107.jpg': { 'name': "Llanowar" },
+    '198109.jpg': { 'name': "Eloren Wilds" },
+    '198110.jpg': { 'name': "Undercity Reaches" },
+    '198111.jpg': { 'name': "Sea of Sand" },
+    '226507.jpg': { 'name': "Orzhova" },
+    '226508.jpg': { 'name': "Reality Shaping", 'phenomenon': true },
+    '226509.jpg': { 'name': "Chaotic AEther", 'phenomenon': true },
+    '226510.jpg': { 'name': "Winriddle Palaces" },
+    '226512.jpg': { 'name': "Akoum" },
+    '226513.jpg': { 'name': "Onakke Catacomb" },
+    '226514.jpg': { 'name': "Morphic Tide", 'phenomenon': true },
+    '226515.jpg': { 'name': "Mount Keralia", 'counter': true },
+    '226517.jpg': { 'name': "Aretopolis", 'counter': true },
+    '226518.jpg': { 'name': "Quicksilver Sea" },
+    '226520.jpg': { 'name': "Mutual Epiphany", 'phenomenon': true },
+    '226521.jpg': { 'name': "Stairs to Infinity" },
+    '226523.jpg': { 'name': "Kilnspire District", 'counter': true },
+    '226524.jpg': { 'name': "Orochi Colony" },
+    '226525.jpg': { 'name': "Talon Gates" },
+    '226526.jpg': { 'name': "Astral Arena" },
+    '226527.jpg': { 'name': "Edge of Malacol" },
+    '226528.jpg': { 'name': "The Zephyr Maze" },
+    '226529.jpg': { 'name': "Trail of the Mage-Rings" },
+    '226532.jpg': { 'name': "Lair of the Ashen Idol" },
+    '226533.jpg': { 'name': "Takenuma" },
+    '226534.jpg': { 'name': "Kharasha Foothills" },
+    '226535.jpg': { 'name': "Glen Elendra" },
+    '226536.jpg': { 'name': "Furnace Layer" },
+    '226537.jpg': { 'name': "Kessig" },
+    '226538.jpg': { 'name': "Selesnya Loft Gardens" },
+    '226539.jpg': { 'name': "Bloodhill Bastion" },
+    '226540.jpg': { 'name': "Norn's Dominion" },
+    '226541.jpg': { 'name': "Grove of the Dreampods" },
+    '226542.jpg': { 'name': "Gavony" },
+    '226543.jpg': { 'name': "Nephalia" },
+    '226544.jpg': { 'name': "Hedron Fields of Agadeem" },
+    '226545.jpg': { 'name': "Planewide Disaster", 'phenomenon': true },
+    '226546.jpg': { 'name': "Spatial Merging", 'phenomenon': true },
+    '226549.jpg': { 'name': "Interplanar Tunnel", 'phenomenon': true },
+    '226551.jpg': { 'name': "Jund" },
+    '226552.jpg': { 'name': "Prahv" },
+    '226553.jpg': { 'name': "Truga Jungle" },
+    '226554.jpg': { 'name': "Grand Ossuary" },
+    '226555.jpg': { 'name': "Stensia" },
+    '226556.jpg': { 'name': "Time Distortion", 'phenomenon': true },
+  },
+  // $.grep(Object.keys(eternity.names), function (key) {
+  //   if (!eternity.names[key].phenomenon) { return key }
+  // });
   'planes': [
     '198062.jpg', '198068.jpg', '198075.jpg', '198081.jpg',
     '198088.jpg', '198098.jpg', '198105.jpg', '226515.jpg',
@@ -20,25 +107,21 @@ var eternity = {
     '226507.jpg', '226523.jpg', '226529.jpg', '226537.jpg',
     '226543.jpg', '226552.jpg'
   ],
-
   'phenomena': [
     '226509.jpg', '226549.jpg', '226514.jpg', '226520.jpg',
     '226545.jpg', '226508.jpg', '226546.jpg', '226556.jpg'
   ],
-
   'counter_planes': {
     '226523.jpg': 0,
     '226517.jpg': 0,
     '226515.jpg': 0,
     '198070.jpg': 0
   },
-
   'cells': [
     '0_3', '-1_2', '0_2', '1_2', '-2_1', '-1_1', '0_1', '1_1', '2_1',
     '-3_0', '-2_0', '-1_0', '0_0', '1_0', '2_0', '3_0', '-2_-1', '-1_-1',
     '0_-1', '1_-1', '2_-1', '-1_-2', '0_-2', '1_-2', '0_-3'
   ],
-
   'map': {},
   'chaotic_aether': 0
 };
@@ -52,7 +135,7 @@ function roll() {
   var result = 0;
   var diceroll = Math.round(Math.random()*5);
 
-  // clear current chaos
+  // clear existing state
   $("#chaos").hide();
 
   // 0-3: blank roll
@@ -75,15 +158,17 @@ function roll() {
 function walk() {
   var walkto = [];
 
-  // clear eternity.chaotic_aether on planeswalk
+  // clear existing state
+  $("#chaos").hide();
   if (eternity.chaotic_aether) {
     eternity.chaotic_aether = 0;
   }
-  var all = eternity.planes.concat(eternity.phenomena);
+
+  var all = Object.keys(eternity.names);
   walkto.push(pick(all));
 
   // encountered a phenomenon
-  while (eternity.phenomena.indexOf(walkto[walkto.length-1]) >= 0) {
+  while (eternity.names[walkto[walkto.length-1]].phenomenon) {
     // Interplanar Tunnel (choose from 5 planes)
     if (walkto[walkto.length-1] == '226549.jpg') {
       var tmp_planes = eternity.planes.slice(0);
