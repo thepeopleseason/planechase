3c812bc2e5dc0bbb28f13b6bbc368ed6c600e17e
jhsiao <jhsiao@rsglab.com>
2020-04-28 20:07:25 -0400

    updated planar counter links

--- planechase.js
+++ planechase.js
@@ -1,93 +1,90 @@
 var eternity = {
-  'names': {
-    '198062.jpg': { 'name': "The Maelstrom" },
-    '198063.jpg': { 'name': "Stronghold Furnace" },
-    '198064.jpg': { 'name': "Panopticon" },
-    '198065.jpg': { 'name': "Izzet Steam Maze" },
-    '198066.jpg': { 'name': "Feeding Grounds" },
-    '198067.jpg': { 'name': "The Hippodrome" },
-    '198068.jpg': { 'name': "Glimmervoid Basin" },
-    '198069.jpg': { 'name': "Tazeem" },
-    '198070.jpg': { 'name': "Naar Isle", 'counter': 0 },
-    '198071.jpg': { 'name': "Grixis" },
-    '198073.jpg': { 'name': "Academy at Tolaria West" },
-    '198074.jpg': { 'name': "Murasa" },
-    '198075.jpg': { 'name': "Sokenzan" },
-    '198076.jpg': { 'name': "Raven's Run" },
-    '198077.jpg': { 'name': "Cliffside Market" },
-    '198078.jpg': { 'name': "Minamo" },
-    '198079.jpg': { 'name': "Sanctum of Serra" },
-    '198080.jpg': { 'name': "Pools of Becoming" },
-    '198081.jpg': { 'name': "Velis Vel" },
-    '198083.jpg': { 'name': "The Dark Barony" },
-    '198084.jpg': { 'name': "Immersturm" },
-    '198085.jpg': { 'name': "Turri Island" },
-    '198086.jpg': { 'name': "Shiv" },
-    '198087.jpg': { 'name': "Goldmeadow" },
-    '198088.jpg': { 'name': "The Great Forest" },
-    '198090.jpg': { 'name': "Agyrem" },
-    '198092.jpg': { 'name': "The Eon Fog" },
-    '198095.jpg': { 'name': "Otaria" },
-    '198096.jpg': { 'name': "Isle of Vesuva" },
-    '198097.jpg': { 'name': "Fields of Summer" },
-    '198098.jpg': { 'name': "The &AElig;ther Flues" },
-    '198099.jpg': { 'name': "Bant" },
-    '198100.jpg': { 'name': "Krosa" },
-    '198101.jpg': { 'name': "Skybreen" },
-    '198102.jpg': { 'name': "The Fourth Sphere" },
-    '198103.jpg': { 'name': "Naya" },
-    '198105.jpg': { 'name': "Lethe Lake" },
-    '198107.jpg': { 'name': "Llanowar" },
-    '198109.jpg': { 'name': "Eloren Wilds" },
-    '198110.jpg': { 'name': "Undercity Reaches" },
-    '198111.jpg': { 'name': "Sea of Sand" },
-    '226507.jpg': { 'name': "Orzhova" },
-    '226508.jpg': { 'name': "Reality Shaping", 'phenomenon': true },
-    '226509.jpg': { 'name': "Chaotic AEther", 'phenomenon': true },
-    '226510.jpg': { 'name': "Winriddle Palaces" },
-    '226512.jpg': { 'name': "Akoum" },
-    '226513.jpg': { 'name': "Onakke Catacomb" },
-    '226514.jpg': { 'name': "Morphic Tide", 'phenomenon': true },
-    '226515.jpg': { 'name': "Mount Keralia", 'counter': 0 },
-    '226517.jpg': { 'name': "Aretopolis", 'counter': 1 },
-    '226518.jpg': { 'name': "Quicksilver Sea" },
-    '226520.jpg': { 'name': "Mutual Epiphany", 'phenomenon': true },
-    '226521.jpg': { 'name': "Stairs to Infinity" },
-    '226523.jpg': { 'name': "Kilnspire District", 'counter': 1 },
-    '226524.jpg': { 'name': "Orochi Colony" },
-    '226525.jpg': { 'name': "Talon Gates" },
-    '226526.jpg': { 'name': "Astral Arena" },
-    '226527.jpg': { 'name': "Edge of Malacol" },
-    '226528.jpg': { 'name': "The Zephyr Maze" },
-    '226529.jpg': { 'name': "Trail of the Mage-Rings" },
-    '226532.jpg': { 'name': "Lair of the Ashen Idol" },
-    '226533.jpg': { 'name': "Takenuma" },
-    '226534.jpg': { 'name': "Kharasha Foothills" },
-    '226535.jpg': { 'name': "Glen Elendra" },
-    '226536.jpg': { 'name': "Furnace Layer" },
-    '226537.jpg': { 'name': "Kessig" },
-    '226538.jpg': { 'name': "Selesnya Loft Gardens" },
-    '226539.jpg': { 'name': "Bloodhill Bastion" },
-    '226540.jpg': { 'name': "Norn's Dominion" },
-    '226541.jpg': { 'name': "Grove of the Dreampods" },
-    '226542.jpg': { 'name': "Gavony" },
-    '226543.jpg': { 'name': "Nephalia" },
-    '226544.jpg': { 'name': "Hedron Fields of Agadeem" },
-    '226545.jpg': { 'name': "Planewide Disaster", 'phenomenon': true },
-    '226546.jpg': { 'name': "Spatial Merging", 'phenomenon': true,
-                    'link': 'merged' },
-    '226549.jpg': { 'name': "Interplanar Tunnel", 'phenomenon': true,
-                    'link': 'load' },
-    '226551.jpg': { 'name': "Jund" },
-    '226552.jpg': { 'name': "Prahv" },
-    '226553.jpg': { 'name': "Truga Jungle" },
-    '226554.jpg': { 'name': "Grand Ossuary" },
-    '226555.jpg': { 'name': "Stensia" },
-    '226556.jpg': { 'name': "Time Distortion", 'phenomenon': true },
+  "names": {
+    "198062.jpg": { "name": "The Maelstrom" },
+    "198063.jpg": { "name": "Stronghold Furnace" },
+    "198064.jpg": { "name": "Panopticon" },
+    "198065.jpg": { "name": "Izzet Steam Maze" },
+    "198066.jpg": { "name": "Feeding Grounds" },
+    "198067.jpg": { "name": "The Hippodrome" },
+    "198068.jpg": { "name": "Glimmervoid Basin" },
+    "198069.jpg": { "name": "Tazeem" },
+    "198070.jpg": { "name": "Naar Isle", "counter": 0 },
+    "198071.jpg": { "name": "Grixis" },
+    "198073.jpg": { "name": "Academy at Tolaria West" },
+    "198074.jpg": { "name": "Murasa" },
+    "198075.jpg": { "name": "Sokenzan" },
+    "198076.jpg": { "name": "Raven's Run" },
+    "198077.jpg": { "name": "Cliffside Market" },
+    "198078.jpg": { "name": "Minamo" },
+    "198079.jpg": { "name": "Sanctum of Serra" },
+    "198080.jpg": { "name": "Pools of Becoming" },
+    "198081.jpg": { "name": "Velis Vel" },
+    "198083.jpg": { "name": "The Dark Barony" },
+    "198084.jpg": { "name": "Immersturm" },
+    "198085.jpg": { "name": "Turri Island" },
+    "198086.jpg": { "name": "Shiv" },
+    "198087.jpg": { "name": "Goldmeadow" },
+    "198088.jpg": { "name": "The Great Forest" },
+    "198090.jpg": { "name": "Agyrem" },
+    "198092.jpg": { "name": "The Eon Fog" },
+    "198095.jpg": { "name": "Otaria" },
+    "198096.jpg": { "name": "Isle of Vesuva" },
+    "198097.jpg": { "name": "Fields of Summer" },
+    "198098.jpg": { "name": "The &AElig;ther Flues" },
+    "198099.jpg": { "name": "Bant" },
+    "198100.jpg": { "name": "Krosa" },
+    "198101.jpg": { "name": "Skybreen" },
+    "198102.jpg": { "name": "The Fourth Sphere" },
+    "198103.jpg": { "name": "Naya" },
+    "198105.jpg": { "name": "Lethe Lake" },
+    "198107.jpg": { "name": "Llanowar" },
+    "198109.jpg": { "name": "Eloren Wilds" },
+    "198110.jpg": { "name": "Undercity Reaches" },
+    "198111.jpg": { "name": "Sea of Sand" },
+    "226507.jpg": { "name": "Orzhova" },
+    "226508.jpg": { "name": "Reality Shaping", "phenomenon": true },
+    "226509.jpg": { "name": "Chaotic AEther", "phenomenon": true },
+    "226510.jpg": { "name": "Winriddle Palaces" },
+    "226512.jpg": { "name": "Akoum" },
+    "226513.jpg": { "name": "Onakke Catacomb" },
+    "226514.jpg": { "name": "Morphic Tide", "phenomenon": true },
+    "226515.jpg": { "name": "Mount Keralia", "counter": 0 },
+    "226517.jpg": { "name": "Aretopolis", "counter": 1 },
+    "226518.jpg": { "name": "Quicksilver Sea" },
+    "226520.jpg": { "name": "Mutual Epiphany", "phenomenon": true },
+    "226521.jpg": { "name": "Stairs to Infinity" },
+    "226523.jpg": { "name": "Kilnspire District", "counter": 1 },
+    "226524.jpg": { "name": "Orochi Colony" },
+    "226525.jpg": { "name": "Talon Gates" },
+    "226526.jpg": { "name": "Astral Arena" },
+    "226527.jpg": { "name": "Edge of Malacol" },
+    "226528.jpg": { "name": "The Zephyr Maze" },
+    "226529.jpg": { "name": "Trail of the Mage-Rings" },
+    "226532.jpg": { "name": "Lair of the Ashen Idol" },
+    "226533.jpg": { "name": "Takenuma" },
+    "226534.jpg": { "name": "Kharasha Foothills" },
+    "226535.jpg": { "name": "Glen Elendra" },
+    "226536.jpg": { "name": "Furnace Layer" },
+    "226537.jpg": { "name": "Kessig" },
+    "226538.jpg": { "name": "Selesnya Loft Gardens" },
+    "226539.jpg": { "name": "Bloodhill Bastion" },
+    "226540.jpg": { "name": "Norn's Dominion" },
+    "226541.jpg": { "name": "Grove of the Dreampods" },
+    "226542.jpg": { "name": "Gavony" },
+    "226543.jpg": { "name": "Nephalia" },
+    "226544.jpg": { "name": "Hedron Fields of Agadeem" },
+    "226545.jpg": { "name": "Planewide Disaster", "phenomenon": true },
+    "226546.jpg": { "name": "Spatial Merging", "phenomenon": true,
+                    "link": "merged" },
+    "226549.jpg": { "name": "Interplanar Tunnel", "phenomenon": true,
+                    "link": "load" },
+    "226551.jpg": { "name": "Jund" },
+    "226552.jpg": { "name": "Prahv" },
+    "226553.jpg": { "name": "Truga Jungle" },
+    "226554.jpg": { "name": "Grand Ossuary" },
+    "226555.jpg": { "name": "Stensia" },
+    "226556.jpg": { "name": "Time Distortion", "phenomenon": true },
   },
-  // $.grep(Object.keys(eternity.names), function (key) {
-  //   if (!eternity.names[key].phenomenon) { return key }
-  // });
   'planes': [
     '198062.jpg', '198068.jpg', '198075.jpg', '198081.jpg',
     '198088.jpg', '198098.jpg', '198105.jpg', '226515.jpg',
@@ -157,7 +154,7 @@ function roll() {
       dice_result = "Roll Planar Die";
       break;
     }
-    $("#dice_button")[0].value = dice_result;
+    $('#dice_button')[0].value = dice_result;
     $('#cost_button')[0].value = parseInt($('#cost_button')[0].value) + 1;
   }
 }
@@ -165,13 +162,7 @@ function roll() {
 function walk(plane) {
   var walkto = [];
 
-  // clear existing state
-  $("#dice_button")[0].value = 'Roll Planar Die';
-  reset_cost();
-  if (eternity.chaotic_aether) {
-    eternity.chaotic_aether = 0;
-  }
-
+  reset_plane();
   if (plane) {
     walkto.push(plane);
   }
@@ -208,25 +199,54 @@ function walk(plane) {
   }
   var output = '';
   for (pl in walkto) {
-    height = (walkto.length > 1) ? 50 : 80
-    var img = '<img src="images/' + walkto[pl] +
-      '" height="' + height + '%">';
+    var img = `<img src="images/${ walkto[pl] }" height="${ (walkto.length > 1) ? 50 : 80 }%">`
     if (walkto.indexOf('226549.jpg') >= 0) {
-      output += '<a href="#" onclick="load(\'' +
-        walkto[pl] + '\')">' + img + '</a>';
+      output += `<a href="#" onclick="load('${ walkto[pl] }')">${ img }</a>`;
       clean('226549.jpg');
     }
     else {
-      output += '<a href="#" onclick="walk();">' + img + '</a>';
-      clean(walkto[pl]);
+      output += `<a href="#"${ get_link(walkto[pl]) }>${ img }</a>`;
     }
+    if (typeof eternity.names[walkto[pl]].counter != 'undefined') {
+      eternity.current_planar_count = eternity.names[walkto[pl]].counter;
+    }
+    else {
+      eternity.current_planar_count = '---';
+    }
+    update_count('count_button', eternity.current_planar_count);
   }
   $("#plane").html(output);
   eternity.history.push(eternity.names[walkto[pl]].name);
 }
 
-function reset_cost() {
+function get_link(plane) {
+  var link = '';
+  if (!eternity.names[plane].phenomenon) {
+    if (typeof eternity.names[plane].counter != 'undefined') {
+      link = ` onclick="add_counter('${ plane }');"`;
+    }
+    else {
+      link = ' onclick="walk();"';
+    }
+  }
+  return link;
+}
+
+function add_counter(plane) {
+  update_count('count_button', ++eternity.current_planar_count);
+}
+
+function reset_plane() {
+  $('#dice_button')[0].value = 'Roll Planar Die';
   $('#cost_button')[0].value = 0;
+  eternity.current_planar_count = 0;
+  if (eternity.chaotic_aether) {
+    eternity.chaotic_aether = 0;
+  }
+}
+
+function update_count(id, count) {
+  $("#" + id)[0].value = count;
 }
 
 function help() {
